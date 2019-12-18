f31c3904ec2dba6d113cf94617b8942b759b0636
Mr. Crank <crank19869@gmail.com>
2019-12-18 10:47:15 +0600

    init dev

---
+++ display_image.py
@@ -0,0 +1,21 @@
+# import the necessary packages
+import argparse
+import os
+
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="path to the image file")
+args = vars(ap.parse_args())
+
+# load the image from disk and display the width, height,
+# and depth
+image = cv2.imread(args["image"])
+(h, w, d) = image.shape
+print("w: {}, h: {}, d: {}".format(w, h, d))
+
+# show the image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
